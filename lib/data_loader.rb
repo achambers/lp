dcae8b152e0847dfa75f4954c195c9f987dc3cc8
Aaron Chambers <achambers@gmail.com>
2012-06-09 18:37:54 +0100

    Refactoring data_loader to clean it up

--- lib/data_loader.rb
+++ lib/data_loader.rb
@@ -4,47 +4,37 @@ module LonelyPlanet
       def load(accommodation_data, traveller_data)
         if accommodation_data
           accommodation = Accommodation.new(accommodation_data)
-          accommodation.attributez = accommodation_attributez(accommodation_data)
-          accommodation.capacity = accommodation_capacity(accommodation_data)
+          accommodation.attributez = attributes(accommodation_data['attributes'])
+          accommodation.capacity = accommodation_capacity(accommodation_data['capacity'])
           accommodation.save
         end
 
         if traveller_data
           traveller = Traveller.new(traveller_data)
-          traveller.price_range = traveller_price_range(traveller_data)
-          traveller.requirements = traveller_requirements(traveller_data)
+          traveller.price_range = traveller_price_range(traveller_data['priceRange'])
+          traveller.requirements = attributes(traveller_data['requirements'])
           traveller.save
         end
       end
 
       private
 
-      def accommodation_attributez attrs
+      def attributes data
         attributes = []
-        attrs['attributes'].each do |attribute_string|
-          attribute = Attribute.first(:name => attribute_string) || Attribute.create(:name => attribute_string)
+        data.each do |entry|
+          attribute = Attribute.first(:name => entry) || Attribute.create(:name => entry)
           attributes << attribute
         end
 
         attributes
       end
 
-      def accommodation_capacity attrs
-        Capacity.create(:total => attrs['capacity']['total'], :free => attrs['capacity']['free'])
+      def accommodation_capacity data
+        Capacity.create(:total => data['total'], :free => data['free'])
       end
 
-      def traveller_price_range attrs
-        PriceRange.create(:min => attrs['priceRange']['min'], :max => attrs['priceRange']['max'])
-      end
-
-      def traveller_requirements attrs
-        requirements = []
-        attrs['requirements'].each do |requirements_string|
-          attribute = Attribute.first(:name => requirements_string) || Attribute.create(:name => requirements_string)
-          requirements << attribute
-        end
-
-        requirements
+      def traveller_price_range data
+        PriceRange.create(:min => data['min'], :max => data['max'])
       end
     end
   end
