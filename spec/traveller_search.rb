6707e23d7f6ad56437df78de4fd175bc27884e5c
Aaron Chambers <achambers@gmail.com>
2012-06-10 13:53:36 +0100

    Added traveller search spec

---
+++ spec/traveller_search.rb
@@ -0,0 +1,38 @@
+current_dir = File.dirname(__FILE__)
+$LOAD_PATH.unshift(current_dir)
+
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'spec_helper'
+require 'json'
+
+module LonelyPlanet
+  class TravellerSearch
+    class << self
+      def search(data, id)
+        data.detect {|traveller| traveller['id'] == id}
+      end
+    end
+  end
+end
+
+describe LonelyPlanet::TravellerSearch do
+  before do
+    file = File.read('dat/travellers.json')
+    @traveller_data = JSON.parse(file)
+  end
+
+  it 'should return a traveller for a valid id' do
+    traveller = LonelyPlanet::TravellerSearch.search(@traveller_data, 12345)
+
+    traveller.wont_be_nil
+    traveller['id'].must_equal 12345
+    traveller['name'].must_equal 'Julian Doherty'
+  end
+
+  it 'should return nil for an invalid traveller id' do
+    traveller = LonelyPlanet::TravellerSearch.search(@traveller_data, 9999)
+
+    traveller.must_be_nil
+  end
+end
