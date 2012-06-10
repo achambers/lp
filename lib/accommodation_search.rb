98c6c6fc03efc20719ea3cdf978dafb9873e93bc
Aaron Chambers <achambers@gmail.com>
2012-06-10 13:03:49 +0100

    Moved AccommodationSearch into its own file

---
+++ lib/accommodation_search.rb
@@ -0,0 +1,26 @@
+module LonelyPlanet
+  class AccommodationSearch
+    class << self
+      def search_accommodation(data, params)
+        data.sort! { |a, b| a['price'] <=> b['price'] }
+
+        match = data.detect do |accommodation|
+          requirements_match?(params, accommodation) && price_match?(params, accommodation) && has_capacity?(accommodation)
+        end
+      end
+
+      private
+      def requirements_match?(params, accommodation)
+        (params[:requirements] - accommodation['attributes']).length == 0
+      end
+
+      def price_match?(params, accommodation)
+        (accommodation['price'] >= params[:price_min]) && (accommodation['price'] <= params[:price_max])
+      end
+
+      def has_capacity?(accommodation)
+        accommodation['capacity']['free'] > 0
+      end
+    end
+  end
+end
--- lib/lib.rb
+++
@@ -1,4 +0,0 @@
-#current_dir = File.dirname(__FILE__)
-#$LOAD_PATH.unshift(current_dir)
-#
-#require 'data_loader'
