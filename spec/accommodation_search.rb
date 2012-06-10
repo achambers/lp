5c683c6827136eed510bdf866f6a92f48507fdb5
Aaron Chambers <achambers@gmail.com>
2012-06-10 09:30:16 +0100

    Added sorting of accommodation data by price and added price match to search

--- spec/accommodation_search.rb
+++ spec/accommodation_search.rb
@@ -10,9 +10,21 @@ module LonelyPlanet
   class AccommodationSearch
     class << self
       def search_accommodation(data, params)
+        data.sort! {|a,b| a['price'] <=> b['price']}
+
         match = data.detect do |accommodation|
-          (params[:requirements] - accommodation['attributes']).length == 0
+          requirements_match?(params, accommodation) &&
+              price_match?(params, accommodation)
         end
+      end
+
+      private
+      def requirements_match?(params, accommodation)
+        (params[:requirements] - accommodation['attributes']).length == 0
+      end
+
+      def price_match?(params, accommodation)
+        (accommodation['price'] >= params[:price_min]) && (accommodation['price'] <= params[:price_max])
       end
     end
   end
@@ -27,17 +39,19 @@ describe LonelyPlanet::AccommodationSearch do
     @traveller_data = JSON.parse(file)
   end
 
-  it 'should find an accommodation when traveller requirements match' do
+  it 'should find an accommodation when traveller requirements and price match' do
     wilson = @traveller_data[2]
 
     search_params = {
-        :requirements => wilson['requirements']
+        :requirements => wilson['requirements'],
+        :price_min => wilson['priceRange']['min'],
+        :price_max => wilson['priceRange']['max']
     }
 
     accommodation = LonelyPlanet::AccommodationSearch.search_accommodation(@accommodation_data, search_params)
 
     accommodation.wont_be_nil
-    accommodation['name'].must_equal 'Little Backpackers'
+    accommodation['name'].must_equal 'Little Backpackers The Second'
   end
 
   it 'should not find an accommodation when a traveller requirements do not match' do
